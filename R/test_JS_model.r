56ac1d69f8e4dc973929f9a8eb65222b977a5f47
bchasco <brandon.chasco@gmail.com>
2025-02-11 10:18:07 -0800

    Co-authored-by: Christopher Kopack <christopherkopack@gmail.com> Co-authored-by: John Best <jkbest@gmail.com> Co-authored-by: Kevin See <KevinSee@users.noreply.github.com>

--- R/test_JS_model.r
+++ R/test_JS_model.r
@@ -1,4 +1,25 @@
-#This is a test model.
-#I'm here.
-#Plot1
-plot(1)
+ns <- 8 #number of strata
+ps <- dpois(1:8, lambda = 3)
+ps <- ps / sum(ps)
+
+N <- 1000 #population size
+
+si <- unlist(apply(rmultinom(N, 1, ps), 
+                    2, 
+                    function(x){ which.max(x) })) 
+
+phi <- 0.8
+p <- 0.3
+ch_i <- list()
+for(i in 1:N){
+    tmp_ch <- 0
+    jcnt <- 1
+    for(j in si[i]:8){
+        if(runif()<phi){
+            tmp_ch[jcnt] <- 1
+        }else{
+            tmp_ch[jcnt] <- 1
+        }
+    }
+    
+}
